dd6b3fae9e9e19ed7d6a201a9930414ef8821a08
Brian Prescott <bp@wave39.com>
2015-10-05 20:52:27 -0400

    Alexandria files added

---
+++ Alexandria/app/src/main/java/it/jaschke/alexandria/ScannerActivity.java
@@ -0,0 +1,45 @@
+package it.jaschke.alexandria;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+public class ScannerActivity extends ActionBarActivity implements ZBarScannerView.ResultHandler {
+    private ZBarScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZBarScannerView(this);    // Programmatically initialize the scanner view
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        String barcode = rawResult.getContents();
+        Log.v("ScannerActivity", barcode);
+        Log.v("ScannerActivity", rawResult.getBarcodeFormat().getName());
+        Intent intent = getIntent();
+        intent.putExtra("barcode", barcode);
+        setResult(RESULT_OK, intent);
+        this.finish();
+    }}
